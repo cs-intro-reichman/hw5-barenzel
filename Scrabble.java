16f89a335886e0639d667afbb0ff143e2eb5fc80
barenzel <bar.enzel@post.runi.ac.il>
2024-12-06 21:58:12 +0200

    completed

--- Scrabble.java
+++ Scrabble.java
@@ -119,6 +119,7 @@ public class Scrabble {
 				if (isWordInDictionary(input)) {
 					score += wordScore(input);
 					System.out.println(input + " earned " + wordScore(input) + " points. Score: " + score + " points");
+					System.out.println();
 					hand = MyString.remove(hand, input);
 				} else {
 					System.out.print("No such word in the dictionary. Try again.");
