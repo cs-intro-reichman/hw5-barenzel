445dfa68d11c3dbb626d17a8bc838427dd3f8cfd
barenzel <bar.enzel@post.runi.ac.il>
2024-12-06 22:12:53 +0200

    completed

--- Scrabble.java
+++ Scrabble.java
@@ -114,12 +114,11 @@ public class Scrabble {
 			if (input.equals(".")) {
 				break;
 			} else if (!MyString.subsetOf(input, hand)) {
-				System.out.println("Invalid word. Try again");
+				System.out.println("Invalid word. Try again.");
 			} else {
 				if (isWordInDictionary(input)) {
 					score += wordScore(input);
 					System.out.println(input + " earned " + wordScore(input) + " points. Score: " + score + " points");
-					System.out.println();
 					hand = MyString.remove(hand, input);
 				} else {
 					System.out.print("No such word in the dictionary. Try again.");
